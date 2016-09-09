a68515c6daf42938a8f50bceb03cc5accf4274e3
mdaminulhaq <mdaminulhaq@gmail.com>
2016-09-08 17:21:36 -0700

    Final version.

--- DPCoinChange/DPCoinChange/DPCoinChange.cpp
+++ DPCoinChange/DPCoinChange/DPCoinChange.cpp
@@ -7,14 +7,48 @@
 using namespace std;
 
 int coins[] = { 1, 5, 10, 20, 25 };
+int coins_size = sizeof(coins) / sizeof(coins[0]);
+
+int result[256] = { -1 };
+
+void init()
+{
+	for (int i = 0; i < 256; i++)
+	{
+		result[i] = -1;
+	}
+}
 
 int makeChange(int amount)
 {
-	return amount;
+	if (amount == 0)
+	{
+		return 0;
+	}
+	if (result[amount] != -1)
+	{
+		return result[amount];
+	}
+
+	int min = INT_MAX;
+	for (int i = 0; i < coins_size; i++)
+	{
+		if (amount - coins[i] >= 0)
+		{
+			int res = makeChange(amount - coins[i]);
+			if (res < min)
+			{
+				min = res;
+			}
+		}
+	}
+	result[amount] = min + 1;
+	return result[amount];
 }
 
 int main()
 {
+	init();
 	cout << "What is amount to change:";
 	int amount;
 	cin >> amount;
